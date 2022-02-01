3a42583b68ca7efee7ab9d8b10d1d95bba070e36
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-02-01 12:40:41 +0600

    Passport-JWT

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose')
 
+
 var User = new Schema({
     admin: {
         type: Boolean,
--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const authenticate = require('../authenticate');
 
 const leaderRouter = express.Router();
 
